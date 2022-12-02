4aa46850108cfa89c4618a7c944e0c3c49f2c965
Devotion <11465401+jstart@user.noreply.gitee.com>
2022-12-02 20:47:37 +0800

    开启微型电商搭建之路!@Wqy

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <router-view/> <!-- 负责显示与路由绑定的所有视图 -->
   </div>
 </template>
 
 <style>
+*{ /* 所有的外边距都为0 */
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
